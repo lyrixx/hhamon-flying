749b06a7bdf76f163571b073dfef509dd6d22100
Grégoire Pineau <lyrixx@lyrixx.info>
2012-06-13 12:06:16 +0200

    Add http cache on main action

--- src/controllers.php
+++ src/controllers.php
@@ -1,14 +1,18 @@
 <?php
 
+use Symfony\Component\HttpFoundation\Response;
+
 $app->get('/', function() use ($app) {
     $params['is_flying'] = false;
     $tweet = $app['tweet.inspector']->getRecentTweetAboutFlying();
 
     if ($tweet) {
-        $oembed = $app['tweet.inspector']->getOembedForTweet($tweet, 500, 'center');
-        $params['oembed_html'] = $oembed->html;
+        $params['oembed_html'] = $app['tweet.inspector']->getOembedForTweet($tweet, 500, 'center')->html;
         $params['is_flying'] = true;
     }
 
-    return $app['twig']->render('index.html.twig', $params);
+    $response = new Response($app['twig']->render('index.html.twig', $params));
+    $response->setTtl($app['tweet.inspector.ttl']);
+
+    return $response;
 });
